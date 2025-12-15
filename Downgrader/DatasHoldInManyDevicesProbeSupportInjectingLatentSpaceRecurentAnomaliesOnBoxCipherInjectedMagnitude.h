5d0d0a26384d6b7c5401f888618048cd7598c684
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-15 07:02:36 +0100

    Update DatasHoldInManyDevicesProbeSupportInjectingLatentSpaceRecurentAnomaliesOnBoxCipherInjectedMagnitude.h

--- Downgrader/DatasHoldInManyDevicesProbeSupportInjectingLatentSpaceRecurentAnomaliesOnBoxCipherInjectedMagnitude.h
+++ Downgrader/DatasHoldInManyDevicesProbeSupportInjectingLatentSpaceRecurentAnomaliesOnBoxCipherInjectedMagnitude.h
@@ -1,1 +1,4 @@
 
+
+//The cipher 106 is the size of the box vontaining latent variables.  LAtent variables cannot overcome 20 digits size.
+//According to those informations, we can overcome anomalies issues if we stop the source probe to exceed recording latent variables (another topic).
